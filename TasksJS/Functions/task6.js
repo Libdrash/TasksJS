cb55a0ded960c9fe1496c577ad3527ddf9fd13eb
Libdrash <Libdrash@gmail.com>
2023-04-02 22:06:29 +0300

    Задача 6

--- TasksJS/Functions/task6.js
+++ TasksJS/Functions/task6.js
@@ -1,8 +1,17 @@
-let sum = (...arguments) => {
+// let sum = (...arguments) => {
+//   let result = 0
+//   for (let i = 0; i < arguments.length; i++) {
+//     result += arguments[i]
+//   }
+//   return result
+// }
+// console.log(sum(4, 5, 6))
+
+function sumAll(...all) {
   let result = 0
-  for (let i = 0; i < arguments.length; i++) {
-    result += arguments[i]
+  for (let num of all) {
+    result += num
   }
   return result
 }
-console.log(sum(4, 5, 6))
+console.log(sumAll(1, 2, 3, 4, 5))
